935573ecd8f6a43ddbbf31b4a2948463771ed43c
Festus Muia <fstsmuia@gmail.com>
2025-06-04 17:04:47 +0300

    phonebook: exercise 3.22 lint config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const Person = require('./models/phonebook')
 
 const app = express()
@@ -10,19 +10,19 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-morgan.token("body", (request) => {
-  return request.method === "POST"? JSON.stringify(request.body) : " "
+morgan.token('body', (request) => {
+  return request.method === 'POST'? JSON.stringify(request.body) : ' '
 })
 
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get("/api/persons", (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons)
-    })
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
-app.get("/info", (request, response) => {
+app.get('/info', (request, response) => {
   Person.countDocuments().then(count => {
     const body = `<p>Phonebook has info for ${count} people</p>
     <p>${new Date()}</p>`
@@ -31,7 +31,7 @@ app.get("/info", (request, response) => {
   })
 })
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     if (person) {
       response.json(person)
@@ -41,22 +41,22 @@ app.get("/api/persons/:id", (request, response, next) => {
   }).catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id).then(result => {
     response.status(204).end()
-  }).catch(error => next(error))  
+  }).catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response, next) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   if (!body.name) {
     return response.status(400).json({
-      error: "name missing"
+      error: 'name missing'
     })
   }
   if (!body.number) {
     return response.status(400).json({
-      error: "number missing"
+      error: 'number missing'
     })
   }
 
@@ -67,11 +67,11 @@ app.post("/api/persons", (request, response, next) => {
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
-  }).catch(error => next(error))  
+  }).catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const {name, number} = request.body
+  const { name, number } = request.body
 
   Person.findById(request.params.id).then(person => {
     if (!person) {
@@ -89,7 +89,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 const errorHandler = (error, request, response, next) => {
--- models/phonebook.js
+++ models/phonebook.js
@@ -7,35 +7,35 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url).then(result => {
-    console.log('connected to MongoDB')
+  console.log('connected to MongoDB')
 }).catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
+  console.log('error connecting to MongoDB:', error.message)
 })
 
 const isPhoneNumberValid = (phoneNumber) => {
-    const pattern = /^\d{2,3}-\d+$/
-    return pattern.test(phoneNumber)
+  const pattern = /^\d{2,3}-\d+$/
+  return pattern.test(phoneNumber)
 }
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: [3, 'Name should be at least 3 characters long'],
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: [8, 'Number should be at least 8 characters long'],
-        validate: [isPhoneNumberValid, 'Invalid phone number format']
-    }
+  name: {
+    type: String,
+    minLength: [3, 'Name should be at least 3 characters long'],
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: [8, 'Number should be at least 8 characters long'],
+    validate: [isPhoneNumberValid, 'Invalid phone number format']
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
